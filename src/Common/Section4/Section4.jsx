de0df0e687bee9c172340441bd8d41ef2bbfe424
PauloAnt <pauloabf235@gmail.com>
2024-11-14 11:37:59 -0300

    Correção Carrosel de projetos

--- src/Common/Section4/Section4.jsx
+++ src/Common/Section4/Section4.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import "./Section4.css";
 import api from "../../axiosConfig.jsx";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination, Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 export default function Section4() {
     const [projects, setProjects] = useState([]);
@@ -36,16 +36,23 @@ export default function Section4() {
                 <Swiper
                     modules={[Pagination, Navigation]}
                     slidesPerView={1}
-                    pagination={{ clickable: true }}
+                    spaceBetween={20} 
                     navigation
+                    breakpoints={{
+                        768: {
+                            slidesPerView: 1, 
+                        },
+                    }}
                 >
                     {projects.map((e) => (
                         <SwiperSlide key={e.id}>
                             <div className="s4-container-project">
                                 <h1>{e.name}</h1>
-                                <img src={e.image} width={450} alt={`Imagem de ${e.name}`} />
+                                <img src={e.image} alt={`Imagem de ${e.name}`} />
                                 <p>{e.description}</p>
-                                <button onClick={() => openRepositoryLink(e.link)}>Ver repositório</button>
+                                <button onClick={() => openRepositoryLink(e.link)}>
+                                    Ver repositório
+                                </button>
                             </div>
                         </SwiperSlide>
                     ))}
